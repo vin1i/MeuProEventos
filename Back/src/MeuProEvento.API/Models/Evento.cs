697cf1b75538fb2a83e3b42ebe7a6f86619ae8c1
Vini <126819626+vin1i@users.noreply.github.com>
2024-06-28 17:11:07 -0300

    Atualização na Pasta "Models".
    Comentário deixado no diretório "Models".

--- Back/src/MeuProEvento.API/Models/Evento.cs
+++ Back/src/MeuProEvento.API/Models/Evento.cs
@@ -1,19 +1,36 @@
 namespace MeuProEvento.API.Models
 {
-
-public class Evento
-{
-    public int EventoId {get; set;}
-
-    public string Local {get; set;}
-
-    public string DataEvento {get; set;}
-    public string Tema {get; set;}
-
-    public int  QtdPessoas {get; set;}
-
-    public string Lote {get; set;}
-
-    public string ImagemURL {get; set;}
+    public class Evento
+    {
+        public int EventoId { get; set; }
+        public string? Local { get; set; }
+        public string? DataEvento { get; set; }
+        public string? Tema { get; set; }
+        public int QtdPessoas { get; set; }
+        public string? Lote { get; set; }
+        public string? ImagemURL { get; set; }
+    }
 }
-}
+
+
+
+/* Dessa forma abaixo, é gerado um aviso que as strings não estão sendo anuláveis. Se trata mais de boas práticas.*/
+// namespace MeuProEvento.API.Models
+// {
+
+// public class Evento
+// {
+//     public int EventoId {get; set;}
+
+//     public string Local {get; set;}
+
+//     public string DataEvento {get; set;}
+//     public string Tema {get; set;}
+
+//     public int  QtdPessoas {get; set;}
+
+//     public string Lote {get; set;}
+
+//     public string ImagemURL {get; set;}
+// }
+// }
