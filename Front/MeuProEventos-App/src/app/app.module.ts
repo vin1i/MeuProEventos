e69f15d26d420acbbda6ee13d4b9db31b8b2d70c
Vini <126819626+vin1i@users.noreply.github.com>
2024-07-04 16:22:40 -0300

    Correção de Bugs no Angular
    Reinstalação do "npm".

--- Front/MeuProEventos-App/src/app/app.module.ts
+++ Front/MeuProEventos-App/src/app/app.module.ts
@@ -6,9 +6,10 @@ import { AppComponent } from './app.component';
 import { EventosComponent } from './eventos/eventos.component';
 import { PalestrantesComponent } from './palestrantes/palestrantes.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
-  declarations: [AppComponent, EventosComponent, PalestrantesComponent],
+  declarations: [AppComponent, EventosComponent, PalestrantesComponent, NavComponent],
 
   imports: [BrowserModule, AppRoutingModule, HttpClientModule, BrowserAnimationsModule],
   providers: [],
