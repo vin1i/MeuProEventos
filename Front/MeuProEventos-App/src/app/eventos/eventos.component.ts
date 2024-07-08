14f0e5d0bae359b256e84a3d86e0a7dde1e6f1f3
Vini <126819626+vin1i@users.noreply.github.com>
2024-07-08 10:59:13 -0300

    Ajustes no Filtro de Busca

--- Front/MeuProEventos-App/src/app/eventos/eventos.component.ts
+++ Front/MeuProEventos-App/src/app/eventos/eventos.component.ts
@@ -8,10 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventosComponent implements OnInit {
   public eventos: any = [];
+  public eventosFiltered: any = []; // Array filtrado
   widthImg: number = 150;
   marginImg: number = 2;
   showImage: boolean = true;
-  filterList: string = '';
 
   constructor(private http: HttpClient) {}
 
@@ -25,8 +25,33 @@ export class EventosComponent implements OnInit {
 
   public getEventos(): void {
     this.http.get('https://localhost:7175/api/eventos').subscribe(
-      (response) => (this.eventos = response),
+      (response) => {
+        this.eventos = response;
+        this.eventosFiltered = [...this.eventos]; // Inicializa o filtro com todos os eventos
+      },
       (error) => console.log(error)
+    );
+  }
+
+  // Função para filtrar os eventos com base no texto de busca
+  filterEventos(filterText: string): void {
+    if (!filterText) {
+      this.eventosFiltered = [...this.eventos]; // Se o filtro estiver vazio, mostra todos os eventos
+    } else {
+      this.eventosFiltered = this.eventos.filter((evento: any) =>
+        this.eventoContainsText(evento, filterText)
+      );
+    }
+  }
+
+  // Função auxiliar para verificar se um evento contém o texto de busca
+  eventoContainsText(evento: any, filterText: string): boolean {
+    return (
+      evento.tema.toLowerCase().includes(filterText.toLowerCase()) ||
+      evento.local.toLowerCase().includes(filterText.toLowerCase()) ||
+      evento.dataEvento.toLowerCase().includes(filterText.toLowerCase()) ||
+      evento.qtdPessoas.toString().includes(filterText) || // Converte para string para busca numérica
+      evento.lote.toString().includes(filterText) // Converte para string para busca numérica
     );
   }
 }
